f019b0f7872a07291fd723ec0c9d295f46e08433
Martin Flower <martin.flower@callistaenterprise.se>
2015-07-22 12:51:24 +0200

    SERVICE-238 - Correct namespace error

--- test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/description/getaggregatedalertinformation/CommonParameters.scala
+++ test/non-functional/gatling/src/test/scala/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/description/getaggregatedalertinformation/CommonParameters.scala
@@ -2,9 +2,9 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.description.
 
 trait CommonParameters {
   val serviceName:String     = "AlertInformation"
-  val urn:String             = "urn:riv:clinicalprocess:activityprescription:actoutcome:GetAlertInformationResponder:2"
+  val urn:String             = "urn:riv:clinicalprocess:healthcond:description:GetAlertInformationResponder:2"
   val responseElement:String = "GetAlertInformationResponse"
-  val responseItem:String    = "medicationMedicalRecord"
+  val responseItem:String    = "alertInformation"
   var baseUrl:String         = if (System.getProperty("baseUrl") != null && !System.getProperty("baseUrl").isEmpty()) {
                                    System.getProperty("baseUrl")
                                } else {
